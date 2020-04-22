11dfda127744762a4ee11f3f224af225342f19c6
josepaniagua <jpanjr@gmail.com>
2020-04-21 18:24:07 -0700

    morgan

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ Go code!
 */
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const server = express();
 const PORT = process.env.PORT || 3000;
+
 server.use(express.json());
 server.use(cors());
+server.use(morgan("tiny"));
+
 
 // Importing Sub-Routes
 const projectsRoutes = require("./routes/projectsRoutes");
